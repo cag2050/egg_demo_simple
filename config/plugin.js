68b92cac8581ac2a735a03711b6b972d7906d829
chen_anguo <cag2050@163.com>
2019-07-12 15:46:49 +0800

    配置不使用use strict

--- config/plugin.js
+++ config/plugin.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 // had enabled by egg
 // exports.static = true;
--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 const { app, assert } = require('egg-mock/bootstrap');
 
